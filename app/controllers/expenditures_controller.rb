63824520a35eefdb498ffe6028671030cf112545
monalisanpereira <monalisanpereira@gmail.com>
2023-11-05 11:20:03 +0900

    add route to create new expense

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,2 +1,20 @@
 class ExpendituresController < ApplicationController
+  def new
+    @expenditure = Expenditure.new(family: Family.find(params[:family_id]))
+  end
+
+  def create
+    @expenditure = Expenditure.new(expenditure_params)
+
+    if @expenditure.save
+      redirect_to family_path(@expenditure.family)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def expenditure_params
+      params.require(:expenditure).permit(:description, :amount, :family_id, :date)
+    end
 end
